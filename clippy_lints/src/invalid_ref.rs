ef1c140f8ad913bea8f54e617c011b2e021f5b01
Jonathan Goodman <goodmanjonathan@sbcglobal.net>
2018-08-13 16:35:22 -0500

    references themselves cannot be null or uninitialized

--- clippy_lints/src/invalid_ref.rs
+++ clippy_lints/src/invalid_ref.rs
@@ -5,15 +5,17 @@ use rustc::ty;
 use rustc::hir::*;
 use crate::utils::{match_def_path, opt_def_id, paths, span_help_and_lint};
 
-/// **What it does:** Checks for creation of references to zeroed or uninitialized memory.
+/// **What it does:** Checks for creation of references with uninitialized or null value.
 ///
-/// **Why is this bad?** Creation of null references is undefined behavior.
+/// **Why is this bad?** Creation of null references and uninitialized references is undefined
+/// behavior, even if they are not dereferenced.
 ///
 /// **Known problems:** None.
 ///
 /// **Example:**
 /// ```rust
 /// let bad_ref: &usize = std::mem::zeroed();
+/// let also_bad_ref: &usize = std::mem::uninitialized();
 /// ```
 declare_clippy_lint! {
     pub INVALID_REF,
@@ -21,9 +23,9 @@ declare_clippy_lint! {
     "creation of invalid reference"
 }
 
-const ZERO_REF_SUMMARY: &str = "reference to zeroed memory";
-const UNINIT_REF_SUMMARY: &str = "reference to uninitialized memory";
-const HELP: &str = "Creation of a null reference is undefined behavior; \
+const ZERO_REF_SUMMARY: &str = "null reference";
+const UNINIT_REF_SUMMARY: &str = "uninitialized reference";
+const HELP: &str = "Creation of a null or uninitialized reference is undefined behavior; \
                     see https://doc.rust-lang.org/reference/behavior-considered-undefined.html";
 
 pub struct InvalidRef;
